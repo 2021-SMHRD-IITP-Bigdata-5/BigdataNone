ef316d8fce424baa3358f9626a08ab09ce611bc8
smhrd <smhrd@DESKTOP-AHD5BDT>
2021-12-02 15:58:16 +0900

    환경 구축 설정 - 이동운

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -2,7 +2,10 @@
 public class Test {
 
 	public static void main(String[] args) {
-
+		// Github 환경설정
+		// 1. JDK 버전
+		// 2. Tomcat 버전
+		// 3. Encodig 방식(utf-g)
 		System.out.println("First Code!");
 		
 	}
