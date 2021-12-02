06a712a4a4a0f29eef17eb218d65166615587edb
smhrd <smhrd@DESKTOP-F2IT1OV>
2021-12-02 16:16:09 +0900

    # WARNING: head commit changed in the meantime
    Github 환경설정, 주의사항 주석 추가

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -2,19 +2,13 @@
 public class Test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		// Github 환경설정
+		// Github 환경설정 (팀원 전부 동일해야됨)
 		// 1. JDK 버전
 		// 2. Tomcat 버전
-		// 3. Encodig 방식(utf-g)
-=======
-//		Github 환경설정
-//		1. JDK 버전
-//		2. Tomcat 버전
-//		3. Encoding 방식(UTF-8)
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-IITP-Bigdata-5/BigdataNone.git
+		// 3. Encoding 방식(UTF-8)
+		// 팀원 2명이상이 같은파일을 수정하고 저장하면 충돌이 일어나므로 주의!
+
 		System.out.println("First Code!");
-		
 	}
 
 }
